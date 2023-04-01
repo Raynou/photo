65a381e998e0afd8a7f113ef01b42570e93ceba4
Raynou <rafaelgame2012@gmail.com>
2023-04-01 00:44:09 +0000

    add try of http request to moodle since nodejs server

--- client/main.js
+++ client/main.js
@@ -1,74 +1,68 @@
-const webcamElement = document.getElementById('webcam');
-const canvasElement = document.getElementById('canvas');
-const snapSoundElement = document.getElementById('snapSound');
-const btnTakePhoto = document.getElementById('takePhoto');
-const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
-const url = "http://localhost:8000/api";
+const webcamElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("canvas");
+const snapSoundElement = document.getElementById("snapSound");
+const btnTakePhoto = document.getElementById("takePhoto");
+const webcam = new Webcam(
+  webcamElement,
+  "user",
+  canvasElement,
+  snapSoundElement
+);
+const url = "http://localhost:8000/login";
 const moodleUrl = "http://localhost:3000/login/index.php";
 
-webcam.start()
-   .then(() =>{
-      console.log("webcam started");
-   })
-   .catch(err => {
-       console.log(err);
-   });
+webcam
+  .start()
+  .then(() => {
+    console.log("webcam started");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-btnTakePhoto.addEventListener('click', async () => {
-    // Disabled for debuggin
-    // const picture = webcam.snap();
-    const body = {
-       uri: 'foo',
-       email: getEmail()
-    };
-    const res = await getToken(body);
-    sendToken(res);
+btnTakePhoto.addEventListener("click", async () => {
+  // Disabled for debuggin
+  // const picture = webcam.snap();
+  const body = {
+    uri: "foo",
+    email: getEmail(),
+  };
+  await login(body);
 });
 
 /**
  * Finds moodle user image by the id and renderizes on the login.
- * @param {number} pictureId 
- * @returns 
+ * @param {number} pictureId
+ * @returns
  */
 const getUserPicture = (pictureId) => {
-    if(!pictureId) { return; }
-    const img = document.getElementById('userPicture');
-    img.src = `http://localhost:3000/moodle/pluginfile.php/5/user/icon/boost/f1?rev=${pictureId}`;
-}
+  if (!pictureId) {
+    return;
+  }
+  const img = document.getElementById("userPicture");
+  img.src = `http://localhost:3000/moodle/pluginfile.php/5/user/icon/boost/f1?rev=${pictureId}`;
+};
 
 /**
- * Sends the uri and the email to localhost:8000/api
+ * Sends the email and the photo uri from the user to the node server        
  * @returns '{pic: pictureOfUser, password: passwordOfUser, id: idOfUser}'
  */
-const getToken = async (body={}) => {
-    const res = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json' },
-        body: JSON.stringify(body)
-    });
-    return res.json();
-}
-
-/**
- * Sends the response of getToken to localhost:3000/moodle/index.php
- * @param {Object} body - the json response of getToken   
- */
-
-const sendToken = async (body={}) => {
-    // TODO: petition rejected by cors
-    await fetch(moodleUrl, {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json' },
-        body: JSON.stringify(body)
-    })
-}
+const login = async (body = {}) => {
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+};
 
 /**
  * Gets the string of the input with the id 'email'.
  * @returns
-*/
+ */
 
 const getEmail = () => {
-    const email = document.getElementById('email');
-    return email.value;
-}
+  const email = document.getElementById("email");
+  return email.value;
+};
--- server/routes/photos.routes.js
+++ server/routes/photos.routes.js
@@ -1,31 +1,59 @@
-const { Router } = require('express');
-const { saveImage } = require('../services/uriHandler.js');
-const { getUserPicture, getUserPassword, getUserId } = require('../services/querys.js');
+const { Router } = require("express");
+const { saveImage } = require("../services/uriHandler.js");
+const {
+  getUserPicture,
+  getUserPassword,
+  getUserId,
+} = require("../services/querys.js");
+const http = require("http");
 const router = Router();
 
+router.route("/login").post(async (req, res) => {
+  const uri = req.body?.uri;
+  const email = req.body?.email;
+  // IMPORTANTE: Sí no cambias el nombre de cada imágen que se va a guardar, entonces esta se va a sobreescribir
+  // saveImage({ uri: uri, path: './test.png' });
+  const userPic = await getUserPicture({ email: email });
+  const userPassword = await getUserPassword({ email: email });
+  const userId = await getUserId({ email: email });
+  const body = {
+    pic: userPic,
+    password: userPassword,
+    id: userId,
+  };
 
-router.route('/api')
-      .post(async (req, res) => {
-        const uri = req.body?.uri
-        const email = req.body?.email
-        // IMPORTANTE: Sí no cambias el nombre de cada imágen que se va a guardar, entonces esta se va a sobreescribir
-        // saveImage({ uri: uri, path: './test.png' });
-        const userPic = await getUserPicture({email: email});
-        const userPassword = await getUserPassword({email: email});
-        const userId = await getUserId({email: email});
-        const response = {
-          pic: userPic,
-          password: userPassword,
-          id: userId
-        }
-        res.send(response)
+  // Correlation between photos up to 80%
+  if (true) {
+    // Make an http petititon to Moodle
+    const requestOptions = {
+      hostname: "localhost",
+      port: 3000,
+      path: "/moodle/login",
+      method: "POST",
+    };
+
+    const externalRequest = http.request(requestOptions, (externalResponse) => {
+      externalResponse.on("end", () => {
+        console.log("Este mensaje se debería imprimir sí la petición a Moodle es exitosa");
+        res.redirect("localhost:3000/moodle/login/index.php");
       });
-router.route('/test')
-      .get((req, res) => {
-        res.send('Test')
-      })
-      .post((req, res) => {
-        console.log(req.body);
-        res.send('Ok')
-      });
+    });
+
+    externalRequest.write(JSON.stringify(body));
+
+    externalRequest.end();
+  } else {
+    /* Login failed */
+    res.end();
+  }
+});
+router
+  .route("/test")
+  .get((req, res) => {
+    res.send("Test");
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.send("Ok");
+  });
 module.exports = router;
